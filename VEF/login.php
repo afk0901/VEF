ba41758a6b3e479645c99629300af8a1fb7bbb1d
Arnar Freyr Kristinsson <arnarfreyr@hive.is>
2016-09-16 09:31:19 +0000

    Verkefni 1

---
+++ VEF/login.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<?php 
+include_once './includes/header.php';
+?>
+<body>
+
+<div class="container">
+
+<?php 
+include_once './includes/banner.php';
+include_once './includes/menu.php';
+ ?>
+
+ <div class="content">
+ 
+<div class="row">
+
+  <div class="loginbox col-lg-12">
+   <p> This is an area for the administrator. Please login here if you are an administrator.</p>
+   <p>The login is not ready yet...But will come here soon!</p>
+  </div>
+  </div>
+
+ </div>
+
+</div>
+
+</body>
+</html>
